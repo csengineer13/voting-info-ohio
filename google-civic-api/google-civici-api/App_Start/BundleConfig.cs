e416c5bf80417205bff616a4274c7a920b9b36c2
unknown <csengineer13@gmail.com>
2015-11-19 15:09:27 -0500

    Fixing CSProj -- setting up bundles

--- google-civic-api/google-civici-api/App_Start/BundleConfig.cs
+++ google-civic-api/google-civici-api/App_Start/BundleConfig.cs
@@ -8,21 +8,33 @@ namespace google_civici_api
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            // JS
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                "~/Scripts/lib/jquery-1.10.2.js",
+                "~/Scripts/lib/bootstrap.js"
+                //"~/Scripts/lib/select2.js",
+                //"~/Scripts/lib/jquery.fileupload/jquery.fileupload.js",
+                //"~/Scripts/lib/jquery.fileupload/vendor/jquery.ui.widget.js",
+                //"~/Scripts/bundle.js"
+                ));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/frontendjs")
+                .Include("~/Toolkit/dist/assets/toolkit/scripts/toolkit.js")
+                .Include("~/Scripts/front-end-bundle.js")
+                .Include("~/Scripts/global-frontend.js")
+                );
+            
+            // CSS
+            bundles.Add(new StyleBundle("~/Content/css")
+                .Include("~/Content/select2.css")
+                .Include("~/Content/bootstrap.css")
+                .Include("~/Content/Site.css")
+                );
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/frontend-css")
+                .Include("~/Content/style.css",
+                "~/Toolkit/dist/assets/toolkit/styles/toolkit.css"
+                ));
         }
     }
 }
