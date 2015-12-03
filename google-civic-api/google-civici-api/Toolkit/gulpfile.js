f9812408c254e6a2cbdd6997b43fe2ebf133be48
unknown <csengineer13@gmail.com>
2015-12-03 12:11:58 -0500

    Getting toolkit to build and serve

--- google-civic-api/google-civici-api/Toolkit/gulpfile.js
+++ google-civic-api/google-civici-api/Toolkit/gulpfile.js
@@ -1,29 +1,45 @@
 'use strict';
 
 // modules
-var assemble = require('fabricator-assemble');
-var browserSync = require('browser-sync');
-var csso = require('gulp-csso');
-var del = require('del');
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var gulpif = require('gulp-if');
-var imagemin = require('gulp-imagemin');
-var prefix = require('gulp-autoprefixer');
-var rename = require('gulp-rename');
-var reload = browserSync.reload;
-var runSequence = require('run-sequence');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var webpack = require('webpack');
+var assemble 	= require('fabricator-assemble'),
+browserify 		= require('browserify'),
+browserSync 	= require('browser-sync'),
+concat 			= require('gulp-concat'),
+csso 			= require('gulp-csso'),
+del 			= require('del'),
+gulp 			= require('gulp'),
+gutil 			= require('gulp-util'),
+gulpif 			= require('gulp-if'),
+imagemin 		= require('gulp-imagemin'),
+prefix 			= require('gulp-autoprefixer'),
+rename 			= require('gulp-rename'),
+reload 			= browserSync.reload,
+runSequence 	= require('run-sequence'),
+sass 			= require('gulp-sass'),
+source 			= require('vinyl-source-stream'),
+streamify 		= require('gulp-streamify'),
+sourcemaps 		= require('gulp-sourcemaps'),
+webpack 		= require('webpack'),
+streamqueue 	= require('streamqueue'),
+uglify 			= require('gulp-uglify'),
+yml 			= require('gulp-yml');
 
 
-// configuration
 var config = {
 	dev: gutil.env.dev,
 	src: {
 		scripts: {
-			fabricator: './src/assets/fabricator/scripts/fabricator.js',
+			fabricator: [
+				'src/assets/fabricator/scripts/prism.js',
+				'src/assets/fabricator/scripts/classList.js',
+				'src/assets/fabricator/scripts/fabricator.js'
+			],
+			vendor: [
+				'node_modules/jquery/dist/jquery.js',
+				'bower_components/jquery-smooth-scroll/jquery.smooth-scroll.js',
+				'bower_components/drawDoughnutChart/jquery.drawDoughnutChart.js',
+				'bower_components/owl.carousel/dist/owl.carousel.js'
+			],
 			toolkit: './src/assets/toolkit/scripts/toolkit.js'
 		},
 		styles: {
@@ -33,18 +49,18 @@ var config = {
 		images: 'src/assets/toolkit/images/**/*',
 		views: 'src/toolkit/views/*.html'
 	},
-	dest: 'dist'
+	dest: 'dist'		
 };
 
 
 // webpack
-var webpackConfig = require('./webpack.config')(config);
-var webpackCompiler = webpack(webpackConfig);
+//var webpackConfig = require('./webpack.config')(config);
+//var webpackCompiler = webpack(webpackConfig);
 
 
 // clean
-gulp.task('clean', function () {
-	return del([config.dest]);
+gulp.task('clean', function (cb) {
+	return del([config.dest], cb);
 });
 
 
@@ -76,20 +92,53 @@ gulp.task('styles', ['styles:fabricator', 'styles:toolkit']);
 
 
 // scripts
-gulp.task('scripts', function (done) {
-	webpackCompiler.run(function (error, result) {
-		if (error) {
-			gutil.log(gutil.colors.red(error));
-		}
-		result = result.toJson();
-		if (result.errors.length) {
-			result.errors.forEach(function (error) {
-				gutil.log(gutil.colors.red(error));
-			});
-		}
-		done();
-	});
+// gulp.task('webpack-scripts', function (done) {
+// 	webpackCompiler.run(function (error, result) {
+// 		if (error) {
+// 			gutil.log(gutil.colors.red(error));
+// 		}
+// 		result = result.toJson();
+// 		if (result.errors.length) {
+// 			result.errors.forEach(function (error) {
+// 				gutil.log(gutil.colors.red(error));
+// 			});
+// 		}
+// 		done();
+// 	});
+// });
+
+gulp.task('scripts:fabricator', function () {
+	return gulp.src(config.src.scripts.fabricator)
+		.pipe(concat('f.js'))
+		.pipe(gulpif(!config.dev, uglify()))
+		.pipe(gulp.dest(config.dest + '/assets/fabricator/scripts'));
 });
+
+gulp.task('scripts:toolkit', function () {
+
+	var toolkit = function () {
+		return browserify(config.src.scripts.toolkit).bundle()
+			.on('error', function (error) {
+				gutil.log(gutil.colors.red(error));
+				this.emit('end');
+			})
+			.pipe(source('toolkit.js'));
+	};
+
+	var vendor = function () {
+		return gulp.src(config.src.scripts.vendor)
+			.pipe(concat('vendor.js'));
+	};
+
+	return streamqueue({ objectMode: true }, vendor(), toolkit())
+		.pipe(streamify(concat('toolkit.js')))
+		.pipe(gulpif(!config.dev, streamify(uglify())))
+		.pipe(gulp.dest(config.dest + '/assets/toolkit/scripts'));
+
+});
+
+gulp.task('scripts', ['scripts:fabricator', 'scripts:toolkit']);
+
 
 
 // images
@@ -106,12 +155,43 @@ gulp.task('favicon', function () {
 
 
 // assemble
-gulp.task('assemble', function (done) {
+// gulp.task('assemble', function (done) {
+// 	assemble({
+// 		logErrors: config.dev
+// 	});
+// 	done();
+// });
+gulp.task('assemble', function(done) {
 	assemble({
-		logErrors: config.dev
+		helpers: {
+			markdown: require('helper-markdown'),
+			is: function(value, test, options) {
+				if (value === test) {
+					return options.fn(this);
+				} else {
+					return options.inverse(this);
+				}
+			},
+			add: function (value, addition) {
+				return value + addition;
+			},
+			default: function (value, defaultValue) {
+				return value ? value : defaultValue;
+			}
+		},
+		logErrors: true
 	});
 	done();
 });
+
+
+// save out data as JSON
+gulp.task('data', function () {
+	return gulp.src('src/data/**/*')
+		.pipe(yml().on('error', gutil.log))
+		.pipe(gulp.dest(config.dest + '/data'));
+});
+
 
 
 // server
@@ -170,6 +250,7 @@ gulp.task('default', ['clean'], function () {
 		'styles',
 		'scripts',
 		'images',
+		'data',
 		'assemble'
 	];
 
--- google-civic-api/google-civici-api/Toolkit/src/assets/fabricator/scripts/fabricator.js
+++ google-civic-api/google-civici-api/Toolkit/src/assets/fabricator/scripts/fabricator.js
@@ -1,7 +1,5 @@
 'use strict';
 
-require('./prism');
-
 /**
  * Global `fabricator` object
  * @namespace
